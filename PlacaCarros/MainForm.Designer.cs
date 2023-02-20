1b4a5ad0b2501b90d9ef0d30a230ffa7fbd7e5d3
Rhuan <rhuan3003@gmail.com>
2023-02-20 20:17:57 -0300

    Adicionanando os arquivos C#

---
+++ PlacaCarros/MainForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Aluno
+ * Date: 10/12/2021
+ * Time: 10:50
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace PlacaCarros
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.BackColor = System.Drawing.Color.White;
+			this.richTextBox1.Location = new System.Drawing.Point(526, 51);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.ReadOnly = true;
+			this.richTextBox1.Size = new System.Drawing.Size(173, 284);
+			this.richTextBox1.TabIndex = 5;
+			this.richTextBox1.Text = "";
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(36, 62);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(164, 58);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Escolha o dia da semana";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.BackColor = System.Drawing.Color.LightGray;
+			this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+			"Segunda-Feira",
+			"Terça-Feira",
+			"Quarta-Feira",
+			"Quinta-Feira",
+			"Sexta-Feira"});
+			this.comboBox1.Location = new System.Drawing.Point(36, 123);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(164, 24);
+			this.comboBox1.TabIndex = 8;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1SelectedIndexChanged);
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(220, 2);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(220, 73);
+			this.label2.TabIndex = 11;
+			this.label2.Text = "Motoristas de folga";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(220, 78);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(220, 264);
+			this.listBox1.TabIndex = 12;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.BackColor = System.Drawing.Color.DimGray;
+			this.ClientSize = new System.Drawing.Size(460, 394);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.richTextBox1);
+			this.Name = "MainForm";
+			this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.Text = "PlacaCarros";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+
+		}
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Label label2;
+	}
+}
---
+++ PlacaCarros/MainForm.cs
@@ -0,0 +1,117 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Aluno
+ * Date: 10/12/2021
+ * Time: 10:50
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PlacaCarros
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		
+			
+		public MainForm()
+		{
+			
+			InitializeComponent();
+			
+			
+		}
+		
+		void MainFormLoad(object sender, EventArgs e){
+	
+		try{
+		richTextBox1.LoadFile("ChapaCarros 1.txt", RichTextBoxStreamType.PlainText);
+		}
+			
+		catch{}
+	
+	}
+		void ComboBox1SelectedIndexChanged(object sender, EventArgs e)
+		{
+						int cont = 0;
+
+						if (comboBox1.SelectedItem.ToString() == "Segunda-Feira"){
+							listBox1.Items.Clear();
+							foreach (string lInteira in richTextBox1.Lines)
+				{
+					string [] dados = lInteira.Split('\t');
+					string nUltimo= dados[0];
+					cont++;
+					if (nUltimo[6] == '1' || nUltimo[6] == '2')
+					{
+			    	 listBox1.Items.Add(dados[1]);
+					}
+				}
+			}
+
+							if (comboBox1.SelectedItem.ToString() == "Terça-Feira"){
+								listBox1.Items.Clear();						
+								foreach (string lInteira in richTextBox1.Lines)
+				{
+					string [] dados = lInteira.Split('\t');
+					string nUltimo = dados[0];
+					cont++;
+					if (nUltimo[6] == '3' || nUltimo[6] == '4')
+					{
+					 listBox1.Items.Add(dados[1]);
+					}
+				}
+			}
+						
+							if (comboBox1.SelectedItem.ToString() == "Quarta-Feira"){
+								listBox1.Items.Clear();
+								foreach (string lInteira in richTextBox1.Lines)
+				{
+					string [] dados = lInteira.Split('\t');
+					string nUltimo = dados[0];
+					cont++;
+					if (nUltimo[6] == '5' || nUltimo[6] == '6')
+					{
+					 listBox1.Items.Add(dados[1]);
+					}
+				}
+			}	
+						
+							if (comboBox1.SelectedItem.ToString() == "Quinta-Feira"){
+								listBox1.Items.Clear();
+								foreach (string lInteira in richTextBox1.Lines)
+				{	
+					string [] dados = lInteira.Split('\t');
+					string nUltimo = dados[0];
+					cont++;
+					if (nUltimo[6] == '7' || nUltimo[6] == '8')
+					{
+					 listBox1.Items.Add(dados[1]);
+					}
+				}
+			}	
+						
+							if (comboBox1.SelectedItem.ToString() == "Sexta-Feira"){
+								listBox1.Items.Clear();
+								foreach (string lInteira in richTextBox1.Lines)
+				{
+					string [] dados = lInteira.Split('\t');
+					string nUltimo = dados[0];
+					cont++;
+					if (nUltimo[6] == '9' || nUltimo[6] == '0')
+					{
+					listBox1.Items.Add(dados[1]);
+					}
+				}
+			}
+						
+		}
+
+	}
+}
